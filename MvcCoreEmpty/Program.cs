4f1e109626c77459dc40608128d9590cbfe9cd24
serraguti <pacoserranox@gmail.com>
2025-01-30 10:05:47 +0100

    Add project files.

---
+++ MvcCoreEmpty/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+//TODO LO QUE INDIQUEMOS SERAN SERVICIOS
+//AÑADIMOS LOS SERVICIOS DE VISTAS Y CONTROLLERS
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+//DEBEMOS INDICAR QUE UTILIZAREMOS RUTAS PARA 
+//CONTROLLERS Y VIEWS
+
+//app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}"
+    );
+
+app.Run();
